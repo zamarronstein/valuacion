9aa12ee2031eec8add657f25594630a955e337ba
Juan Manuel Mauricio Zamarrón <zamarronstein@gmail.com>
2023-12-13 02:03:40 -0600

    Se agrega el ISSUER_URI con spring.security.oauth2.authorizationserver.issuer

--- src/main/java/nmp/com/mx/valuacion/config/ResourceServerConfig.java
+++ src/main/java/nmp/com/mx/valuacion/config/ResourceServerConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtDecoders;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
